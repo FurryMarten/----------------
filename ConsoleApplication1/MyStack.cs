f73fffd98c2a8ab616629108d7818393df3b4073
Куница <nik3699@ya.ru>
2020-04-30 17:29:12 +0300

    Initial commit

---
+++ ConsoleApplication1/MyStack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{   
+    class MyStack<T>
+    {
+        T[] mass;
+        int count = 0;
+
+        public MyStack (int length)
+        {
+            mass = new T[length];
+        }
+
+        public void Push (T value)
+        {
+            if (count>=mass.Length)throw new Exception ("Стек переполнен");
+            mass [count++]=value;
+        }
+
+        public T Pop()
+        {
+            if (count == 0)
+                throw new Exception("Стек пуст,данных нет");
+            return mass[--count];
+        }
+
+        public T Top()
+        {
+            return mass[count-1];
+        }
+
+        public bool IsEmpty()
+        {
+            //if (count == 0) return true;
+            //else return false;
+            return (count == 0);
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<int> st = new MyStack<int>(5);
+            //st.Push(15);
+            //st.Push(36);
+            //st.Push(55);
+            //Console.WriteLine(st.Top());
+            //Console.WriteLine(st.Pop());
+            //Console.WriteLine(st.Top());
+            //Console.WriteLine(st.Pop());
+
+            MyStack<char> st1 = new MyStack<char>(15);
+            String s = Console.ReadLine();
+            bool error = false;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+                    st1.Push(s[i]);
+                if (s[i] == ')' || s[i] == '}' || s[i] == ']')
+                {
+                    
+                    if (st1.IsEmpty())  
+                    {
+                        Console.WriteLine("Лишняя закрывающая!");
+                        error = true;
+                        break;
+                    }
+                    
+                    char tmp = st1.Pop();
+                    if (tmp == '{' && s[i] != '}')
+                    {
+                        Console.WriteLine("Типы скобок не совпадают!");
+                        error = true;
+                        break;
+                    }
+                    if (tmp == '[' && s[i] != ']')
+                    {
+                        Console.WriteLine("Типы скобок не совпадают!");
+                        error = true;
+                        break;
+                    }
+                    if (tmp == '(' && s[i] != ')')
+                    {
+                        Console.WriteLine("Типы скобок не совпадают!");
+                        error = true;
+                        break;
+                    }
+                }
+            }
+            if (st1.IsEmpty() && !error)  Console.WriteLine("Все верно");
+            else Console.WriteLine("Не верно");
+        }
+    }
+}
